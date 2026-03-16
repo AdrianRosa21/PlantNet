39dd490760467e5a0304314816ecbc905d5fd40e
Bulk Rosa <bulkrosa24@gmail.com>
2026-03-16 20:27:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9525092173-1991a",
+  "appId": "1:592476351919:web:74ec1a050ea9929213d35a",
+  "apiKey": "AIzaSyDzglhsGGXTT0S6iMCZA4Ci-cQfxAH_3t8",
+  "authDomain": "studio-9525092173-1991a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "592476351919"
+};
